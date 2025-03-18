6285776fa153ec090e1a43f752dd6c70b70031a3
simonelupone <simone.lupone@gmail.com>
2025-03-18 15:17:25 +0100

    [DONE]: add loop for display numbers or string sequence in console

--- script/main.js
+++ script/main.js
@@ -2,3 +2,16 @@
 let sequence = '';
 let sequenceLength = 100;
 let value = 1;
+
+// inizio un ciclo per stampare 100 stringhe
+// che abbiano il valore della loro posizione
+for (i = 1; i <= sequenceLength; i++) {
+
+    sequence += `${value++} `;
+    if ((value % 3) == 0) {
+        sequence += 'Fizz ';
+    } else if ((value % 5) == 0) {
+        sequence += 'Buzz ';
+    }
+}
+console.log(sequence);
