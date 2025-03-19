76caf8b7193ef89507974400b5e8d15e631ad287
simonelupone <simone.lupone@gmail.com>
2025-03-19 09:49:10 +0100

    [fix]: now display numbers and strings correctly minor changes and refactoring

--- script/main.js
+++ script/main.js
@@ -1,17 +1,19 @@
 // definisco le variabili
+const sequenceLength = 100;
 let sequence = '';
-let sequenceLength = 100;
-let value = 1;
 
 // inizio un ciclo per stampare 100 stringhe
 // che abbiano il valore della loro posizione
-for (i = 1; i <= sequenceLength; i++) {
-
-    sequence += `${value++} `;
-    if ((value % 3) == 0) {
+for (let value = 1; value <= sequenceLength; value++) {
+    if (value % 15 === 0) {
+        sequence += 'FizzBuzz ';
+    } else if (value % 3 === 0) {
         sequence += 'Fizz ';
-    } else if ((value % 5) == 0) {
+    } else if (value % 5 === 0) {
         sequence += 'Buzz ';
+    } else {
+        sequence += `${value} `;
     }
 }
+
 console.log(sequence);
